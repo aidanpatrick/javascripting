e6e9bff0eed86e21f638ca99bd1f32083068bc21
Aidan Patrick <aidanpatrick@seattleacademy.org>
2016-02-05 18:12:27 -0800

    complete object-properties.js

--- object-properties.js
+++ object-properties.js
@@ -1,19 +1,23 @@
  /*
-  ## The challenge:
+   ## The challenge:
 
-   Create a file named object-properties.js.
+    Create a file named object-properties.js.
 
-   In that file, define a variable named food like this:
+    In that file, define a variable named food like this:
 
-      var food = {
-        types: 'only pizza'
-      };
+       var food = {
+         types: 'only pizza'
+       };
 
-   Use console.log() to print the types property of the food object to the
-   terminal.
+    Use console.log() to print the types property of the food object to the
+    terminal.
 
-   Check to see if your program is correct by running this command:
+    Check to see if your program is correct by running this command:
 
-      javascripting verify object-properties.js
- */
+       javascripting verify object-properties.js
+  */
 
+ var food = {
+     types: 'only pizza'
+ };
+ console.log(food.types);
